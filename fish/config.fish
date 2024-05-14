a14bc70cbbbd8d36494258286015b988f3e8b7c3
Juniorbasck <adilson.juniorcomunicacao@gmail.com>
2024-05-14 00:06:43 -0300

    first commit

---
+++ fish/config.fish
@@ -0,0 +1,15 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+export PATH="$PATH:/opt/nvim-linux64/bin"
+
+export PATH="$PATH:/usr/local/android-studio-2023.3.1.18-linux/android-studio/bin"
+
+#my alias
+alias update='sudo apt update && sudo apt upgrade'
+alias estudos='cd ~/Adilson/Pessoal/estudos'
+alias faculdade='cd ~/Adilson/Pessoal/faculdade'
+alias s='exit'
+alias v='cd ..'
+
+starship init fish | source
