0b1b2b25c1c108db46ed4641a8ab9d33cdedec32
Juniorbasck <adilson.juniorcomunicacao@gmail.com>
2024-05-17 16:19:11 -0300

    atualizacoes

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -6,8 +6,9 @@ return {
      config = function()
      local builtin = require('telescope.builtin')
       	vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-        vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+        vim.keymap.set("n", "<leader>ft", builtin.live_grep, {}) 
 	      vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+        vim.keymap.set("n", "<leader><leader>", builtin.oldfiles, {})
 	      vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
     end
   },
