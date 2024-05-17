13015dfc9a2325d129ff2f36aa47636d124ea3be
Juniorbasck <adilson.juniorcomunicacao@gmail.com>
2024-05-17 13:48:28 -0300

    atualizacoes

--- fish/config.fish
+++ fish/config.fish
@@ -5,12 +5,14 @@ export PATH="$PATH:/opt/nvim-linux64/bin"
 
 export PATH="$PATH:/usr/local/android-studio-2023.3.1.18-linux/android-studio/bin"
 
-#my alias
 alias update='sudo apt update && sudo apt upgrade'
 alias estudos='cd ~/Adilson/Pessoal/estudos'
 alias faculdade='cd ~/Adilson/Pessoal/faculdade'
+alias profissional='cd ~/Adilson/Profissional/'
+alias trabalho='cd ~/Adilson/Profissional/trabalho'
 alias s='exit'
 alias v='cd ..'
 alias t='tmux'
+alias f='fish'
 
 starship init fish | source
