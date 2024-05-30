17a2d00dc33f0e95b18a953e5d759abfcb3eeb57
Juniorbasck <adilson.juniorcomunicacao@gmail.com>
2024-05-30 18:34:24 -0300

    addlazyGit

---
+++ nvim/lua/plugins/lazy-git.lua
@@ -0,0 +1,16 @@
+return {
+  "kdheepak/lazygit.nvim",
+  cmd = {
+    "LazyGit",
+    "LazyGitConfig",
+    "LazyGitCurrentFile",
+    "LazyGitFilter",
+    "LazyGitFilterCurrentFile",
+  },
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  config = function()
+      vim.keymap.set("<leader>lg", "<cmd>LazyGit<cr>", {})
+  end,
+}
