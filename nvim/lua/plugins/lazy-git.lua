a39aadc08ec9897931cde959970e576edb664347
Juniorbasck <adilson.juniorcomunicacao@gmail.com>
2024-07-18 19:49:16 -0300

    factor file "lazy-git"

--- nvim/lua/plugins/lazy-git.lua
+++ nvim/lua/plugins/lazy-git.lua
@@ -1,11 +1,9 @@
 return {
-  "kdheepak/lazygit.nvim",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-  },
-  config = function()
-    -- Configurando o atalho para abrir o LazyGit
-    vim.api.nvim_set_keymap('n', '<leader>lg', '<cmd>LazyGit<CR>', { noremap = true, silent = true })
-  end,
+	"kdheepak/lazygit.nvim",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+	},
+	config = function()
+		vim.api.nvim_set_keymap("n", "<leader>lg", "<cmd>LazyGit<CR>", { noremap = true, silent = true })
+	end,
 }
-
